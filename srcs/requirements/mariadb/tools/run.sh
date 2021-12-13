98b3919810a8f122b315e5383f8d1d76d1faea1e
Jeongwook Lee <jeongwle@c5r3s3.42seoul.kr>
2021-12-13 13:54:55 +0900

    just push

--- srcs/requirements/mariadb/tools/run.sh
+++ srcs/requirements/mariadb/tools/run.sh
@@ -5,3 +5,5 @@ echo "CREATE DATABASE IF NOT EXISTS '$MYSQL_DATABASE';" | mysql -u root --skip-p
 echo "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" | mysql -u -root --skip-password
 echo "GRANT ALL ON '$MYSQL_DATABASE'.* TO '$MYSQL_USER'@'%';" | mysql -u root --skip-password
 echo "FLUSH PRIVILEGES;" | mysql -u root --skip-password
+
+mysqld_safe
