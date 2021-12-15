8eeb30f4b8ceb6996d9932e95aa92c8286bc1fad
Jeongwook Lee <jeongwle@c5r2s7.42seoul.kr>
2021-12-15 21:36:48 +0900

    mysql error many many too many I cannot fix... T.T

--- srcs/requirements/mariadb/tools/run.sh
+++ srcs/requirements/mariadb/tools/run.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+sudo touch /var/run/mysqld/mysqld.sock
+chmod -R 777 /var/lib/mysql/
+chmod -R 777 /var/run/mysqld/
+chmod -R 777 /var/log/mysql/
+chown -R mysql:mysql /var/lib/mysql/
 service mysql start
-echo "CREATE DATABASE IF NOT EXISTS '$MYSQL_DATABASE';" | mysql -u root --skip-password
-echo "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" | mysql -u -root --skip-password
-echo "GRANT ALL ON '$MYSQL_DATABASE'.* TO '$MYSQL_USER'@'%';" | mysql -u root --skip-password
+echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;" | mysql -u root --skip-password
+echo "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" | mysql -u root --skip-password
+echo "GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';" | mysql -u root --skip-password
 echo "FLUSH PRIVILEGES;" | mysql -u root --skip-password
 
-mysqld_safe --bind-address=0.0.0.0
+mysqld_safe
