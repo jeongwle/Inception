3f4beacd7d6914c12947b124757765ce34fcb2d7
Jeongwook Lee <jeongwle@c10r1s1.42seoul.kr>
2021-12-16 19:34:04 +0900

    Modify : 일단 돌아가는 것은 OK To do : 컨테이너 크래시를 내서 리스타팅 하는지 보여주는 법 알아보기 가상머신위에서 돌려보기. 그리고 호스트네임 바꾸기

--- srcs/requirements/mariadb/tools/run.sh
+++ srcs/requirements/mariadb/tools/run.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
-sudo touch /var/run/mysqld/mysqld.sock
-chmod -R 777 /var/lib/mysql/
-chmod -R 777 /var/run/mysqld/
-chmod -R 777 /var/log/mysql/
-chown -R mysql:mysql /var/lib/mysql/
 service mysql start
+sudo chmod -R 777 /var/lib/mysql/
+sudo chmod -R 777 /var/run/mysqld/
+sudo chmod -R 777 /var/log/mysql/
+sudo chown -R mysql:mysql /var/lib/mysql/
+sudo chmod -R 644 /var/run/mysqld/mysqld.sock
+sleep 1
 echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;" | mysql -u root --skip-password
+sleep 1
 echo "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" | mysql -u root --skip-password
+sleep 1
 echo "GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';" | mysql -u root --skip-password
+sleep 1
 echo "FLUSH PRIVILEGES;" | mysql -u root --skip-password
+sleep 1
+service mysql stop
+sleep 1
 
 mysqld_safe
